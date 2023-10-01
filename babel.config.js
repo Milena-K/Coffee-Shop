06043f87ee56d1c4a50fc5cbc06d070db9de9df5
Milena Kukolj <mkukolj@protonmail.com>
2023-10-02 00:58:08 +0200

    Babel config fix.

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,7 @@
 module.exports = function (api) {
   return {
-    plugins: ['macros'],
+    presets: ['@babel/preset-env', '@babel/preset-react'],
+    plugins: ['@babel/plugin-transform-runtime','macros'],
+    cache: true,
   }
 }
